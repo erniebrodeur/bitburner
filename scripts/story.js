eff4cf9478bf5448c72aae6175fa1f3e58f31acd
Ernie Brodeur <ebrodeur@ujami.net>
2022-03-17 22:22:54 -0700

    quickly bang out story shit

---
+++ scripts/story.js
@@ -0,0 +1,13 @@
+export async function main(ns) {
+      ns.exec("break-host.js", "home", 10, "n00dles")
+      ns.exec("break-host.js", "home", 10, "foodnstuff")
+      ns.exec("break-host.js", "home", 10, "sigma-cosmetics")
+      ns.exec("break-host.js", "home", 10, "joesguns")
+      ns.exec("break-host.js", "home", 10, "harakiri-sushi")
+      ns.exec("break-host.js", "home", 10, "hong-fang-tea")
+      ns.exec("break-host.js", "home", 10, "iron-gym")
+      ns.exec("break-host.js", "home", 10, "CSEC")
+      ns.exec("break-host.js", "home", 10, "I.I.I.I")
+      ns.exec("break-host.js", "home", 10, "avmnite-02h")
+      ns.exec("break-host.js", "home", 10, "run4theh111z")
+}
