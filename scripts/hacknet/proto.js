e41dc8330dc2977621b1b6235ef631af50152931
Ernie Brodeur <ebrodeur@ujami.net>
2022-03-31 02:34:38 -0700

    added an option for not saving reserves.

--- scripts/hacknet/proto.js
+++ scripts/hacknet/proto.js
@@ -1,7 +1,16 @@
+import optionParser from './lib/opt-parser'
+
 /** @param {NS} ns **/
 export async function main(ns) {
+  let options = optionParser(ns.args)
+  let reservePercentage = 0.1
+
+  if (options["no-reserves"] == true) {
+    reservePercentage = 1.0
+  }
+
   while (true) {
-    if (ns.hacknet.numHashes() > (ns.hacknet.hashCapacity() - ns.hacknet.hashCapacity() * 0.1)) {
+    if (ns.hacknet.numHashes() > (ns.hacknet.hashCapacity() - ns.hacknet.hashCapacity() * reservePercentage)) {
       ns.hacknet.spendHashes('Sell for Money')
     }
     await ns.sleep(50)
