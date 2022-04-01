23bba8837736979e808193450bde22c9520224dc
Ernie Brodeur <ebrodeur@ujami.net>
2022-04-01 00:59:18 -0700

    * refactoring * fixed a bug in how index was determined

--- scripts/hacknet/upgrade-nodes.js
+++ scripts/hacknet/upgrade-nodes.js
@@ -1,49 +1,39 @@
-/** @param {NS} ns **/
-export async function main(ns) {
+import optionParser from './lib/opt-parser'
 
+/** @param {NS} main_ns **/
+export async function main(ns) {
   let upgradeCoresAt = [10, 12, 13, 14, 18, 22, 24]
 
-
-
   while (true) {
+    purchaseUpgrade(ns, findTarget(ns))
     await ns.sleep(10)
-    let nodeList = []
-    buildNodeList(ns, nodeList)
-
-    // next lets determine our target
-    let sortedList = nodeList.sort(function (a, b) {
-      return a.production - b.production
-    })
-    let target = sortedList[0]
-
-    // this ugly ass kludge is to find the cheapest upgrade cost
-    // on the target node.
-    let sortedUpgrades = Object.entries(target.upgradeCosts).sort(function (a, b) {
-      return a[1] - b[1]
-    })
-
-    let targetUpgrade = sortedUpgrades[0][0]
-
-    // and now . . . indexes.
-    let targetIndex = target.name[target.name.length - 1]
-
-    // and this lovely shit is because the ns.hacknet api isn't consistent.
-    switch (targetUpgrade) {
-      case 'level':
-        ns.hacknet.upgradeLevel(targetIndex)
-        break
-      case 'ram':
-        ns.hacknet.upgradeRam(targetIndex)
-        break
-      case 'core':
-        ns.hacknet.upgradeCore(targetIndex)
-        break
-    }
   }
 }
 
-function buildNodeList(ns, nodeList) {
+// simple (shitty) method to find the lowest producing -> cheapest upgrade.
+function findTarget(ns) {
+  let nodeList = buildNodeList(ns)
+
+  // next lets determine our target
+  let sortedList = nodeList.sort(function (a, b) {
+    return a.production - b.production
+  })
+  let target = sortedList[0]
+
+  // what we want to upgrade
+  let sortedUpgrades = Object.entries(target.upgradeCosts).sort(function (a, b) {
+    return a[1] - b[1]
+  })
+
+  target.upgrade = sortedUpgrades[0][0]
+  target.index = target.name.match(/^.*-(\d+)$/)[1]
+
+  return target
+}
+
+function buildNodeList(ns) {
   let numberOfNodes = ns.hacknet.numNodes()
+  let nodeList = []
 
   // we need to know the upgrade costs of everything, lets get all the nodes
   // and add it as we go
@@ -54,5 +44,20 @@ function buildNodeList(ns, nodeList) {
       ram: ns.hacknet.getRamUpgradeCost(i, 1),
       core: ns.hacknet.getCoreUpgradeCost(i, 1)
     }
+  }
+  return nodeList
+}
+
+function purchaseUpgrade(ns, target) {
+  switch (target.upgrade) {
+    case 'level':
+      ns.hacknet.upgradeLevel(target.index)
+      break
+    case 'ram':
+      ns.hacknet.upgradeRam(target.index)
+      break
+    case 'core':
+      ns.hacknet.upgradeCore(target.index)
+      break
   }
 }
